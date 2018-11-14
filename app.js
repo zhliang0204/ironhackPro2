325ba8e16ebeae54f15eccfd74fbb96267eec2cd
Luna Zhang <zhliang0204@163.com>
2018-11-15 00:50:00 +0100

    deploy test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const flash      = require("connect-flash");
 
 mongoose
   // .connect('mongodb://localhost/foodshare', {useNewUrlParser: true})
-  .connect(MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
