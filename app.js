92da2e0720db411d3ecc7b2d970c4633e5e0e416
binunair005 <biunair005@gmail.com>
2018-11-15 10:00:09 +0100

    layout

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/foodshare', {useNewUrlParser: true})
+  // .connect('mongodb://localhost/foodshare', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
